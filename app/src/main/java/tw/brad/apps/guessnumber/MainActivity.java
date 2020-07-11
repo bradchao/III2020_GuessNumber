2832d5b74007e3ce1cab5c7ef348e8d472097615
bradchao <bk400288>
2020-07-11 17:36:58 +0800

    0711105

--- app/src/main/java/tw/brad/apps/guessnumber/MainActivity.java
+++ app/src/main/java/tw/brad/apps/guessnumber/MainActivity.java
@@ -3,6 +3,8 @@ package tw.brad.apps.guessnumber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void guess(View view) {
+    }
+
+    public void newGame(View view) {
+    }
+
+    public void setting(View view) {
+    }
+
+    public void exit(View view) {
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.v("bradlog", "onBackPressed before");
+        super.onBackPressed();
+        Log.v("bradlog", "onBackPressed after");
+    }
+
+    @Override
+    public void finish() {
+        Log.v("bradlog", "finish before");
+        super.finish();
+        Log.v("bradlog", "finish after");
     }
 }
