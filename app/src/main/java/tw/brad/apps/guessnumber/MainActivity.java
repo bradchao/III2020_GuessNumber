68128de51e80f8d7b17c75680fb82e56e876d43a
bradchao <bk400288>
2020-07-18 09:58:45 +0800

    071802

--- app/src/main/java/tw/brad/apps/guessnumber/MainActivity.java
+++ app/src/main/java/tw/brad/apps/guessnumber/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Collections;
@@ -13,18 +14,37 @@ import java.util.LinkedList;
 public class MainActivity extends AppCompatActivity {
     private long lastTime = 0;
     private String answer;
+    private EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = findViewById(R.id.input);
+
     }
 
     public void guess(View view) {
+        String strInput = input.getText().toString();
+        String result = checkAB(strInput);
+        Log.v("bradlog", strInput + "=>" + result);
+    }
+
+    private String checkAB(String guess){
+        int A, B; A = B = 0;
+        for (int i=0; i<guess.length(); i++){
+            if (guess.charAt(i) == answer.charAt(i)){
+                A++;
+            }else if (answer.indexOf(guess.charAt(i)) >= 0){
+                B++;
+            }
+        }
+        return A +"A" + B + "B";
     }
 
     public void newGame(View view) {
-        answer = createAnswer(4);
+        answer = createAnswer(3);
         Log.v("bradlog", answer);
     }
 
